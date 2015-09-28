304e8ff88758e3c21b80bf9d9b8f89ff0d14624e
Chidiebere Nnadi <chidiebere.nnadi@gmail.com>
2015-09-28 02:13:21 +0100

    Added in tests

---
+++ test/com/afropolymath/db/ActiveRecJTest.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.afropolymath.db;
+
+import java.sql.ResultSet;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author chidieberennadi
+ */
+public class ActiveRecJTest {
+    
+    public ActiveRecJTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of createConnection method, of class ActiveRecJ.
+     */
+    @Test
+    public void testSample() throws Exception {
+        System.out.println("Sample Test");
+        assertEquals(true, true);
+    }
+}
